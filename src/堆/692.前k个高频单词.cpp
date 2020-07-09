4de47722a3ae7b1cefa79ca7ce340fd0c305d56a
zhangfan <1647508697@qq.com>
2020-07-09 21:39:15 +0800

    7.9

--- src/堆/692.前k个高频单词.cpp
+++ src/堆/692.前k个高频单词.cpp
@@ -7,24 +7,23 @@
 // @lc code=start
 class Solution {
 public:
-    struct cmp{
-        bool operator() (pair<string,int> &a,pair<string,int> &b){
-            if(a.second==b.second)
-                return a.first>b.first;
-            return a.second<b.second;
-        }
-    };
+    static bool cmp(const pair<string,int> &a,const pair<string,int> &b){
+        if(a.second==b.second)
+            return a.first<b.first;
+        return a.second>b.second;
+    }
     vector<string> topKFrequent(vector<string>& words, int k) {
-        priority_queue<pair<string,int>,vector<pair<string,int> >,cmp > q;
+        // priority_queue<pair<string,int>,vector<pair<string,int> >,cmp > q;
+        vector<pair<string,int> > v;
         map<string,int> mp;
         for(string s:words)
             mp[s]++;
         for(auto p:mp)
-            q.push({p.first,p.second});
+            v.push_back({p.first,p.second});
+        sort(begin(v),end(v),cmp);
         vector<string> res;
         for(int i=0;i<k;i++){
-            res.push_back((q.top()).first);
-            q.pop();
+            res.push_back(v[i].first);
         }
         return res;
     }
